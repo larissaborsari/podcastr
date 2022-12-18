fc9d6077ff9a6fd9624f7bb2a222234cd14bcd58
Larissa Borsari <larissaborsari@hotmail.com>
2022-12-18 13:19:36 -0300

    last changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import Header from '../components/Header';
 import "../styles/global.scss"
+import styles from '../styles/app.module.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header></Header>
+        <Component {...pageProps} />
+      </main>
+    </div>
+    
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-        <Header></Header>
+       <p>index</p>
     </>
   )
 }
