03d0085c846683c3760d2f85838ea8a418452d41
Larissa Borsari <larissaborsari@hotmail.com>
2023-01-07 10:21:52 -0300

    last changes in styles

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss';
 
 export default function Player() {
     return (
-        <div>
+        <div className={styles.playerContainer}>
             
         </div>
     );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,16 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import "../styles/global.scss"
 import styles from '../styles/app.module.scss'
+import Player from '../components/Player';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={styles.wrapper}>
       <main>
-        <Header></Header>
+          <Header/>
         <Component {...pageProps} />
       </main>
+      <Player/>
     </div>
     
   );
