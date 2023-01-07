5be3fd1af854fe7d16073369f8b9726a8ba410c2
Larissa Borsari <larissaborsari@hotmail.com>
2023-01-07 10:51:26 -0300

    styles inplayer component

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -4,7 +4,19 @@ import styles from './styles.module.scss';
 export default function Player() {
     return (
         <div className={styles.playerContainer}>
-            
+
+            <header>
+                <img src="/playing.png" alt="Tocando agora" width="30px" />
+                <strong>Tocando Agora</strong>
+            </header>
+
+            <div className={styles.emptyPlayer}>
+                <strong>Selecione um podcast para ouvir</strong>
+            </div>
+
+            <footer>
+
+            </footer>
         </div>
     );
 };
