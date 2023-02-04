27da091df79d3ec05d1866046229943e151a7ac6
Larissa Borsari <larissaborsari@hotmail.com>
2023-02-04 13:33:37 -0300

    api settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import { Inter } from '@next/font/google'
+import { useEffect } from 'react'
 import Header from '../components/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function Home(props: any) {
   return (
     <>
        <p>index</p>
+       <p>{JSON.stringify(props.episodes)}</p>
     </>
-  )
-}
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
+};
