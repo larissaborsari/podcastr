3bf158e629627e85e2bcea5ba507ed737e939c56
Larissa Borsari <larissaborsari@hotmail.com>
2023-03-02 22:38:46 -0300

    add axios config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Inter } from '@next/font/google'
 import { GetStaticProps  } from 'next';
+import { api } from '../services/api';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,8 +27,13 @@ export default function Home(props: HomeProps) {
 };
 
 export  const  getStaticProps: GetStaticProps= async() =>{
-  const response = await fetch('http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc');
-  const data = await response.json();
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
 
   return {
     props: {
