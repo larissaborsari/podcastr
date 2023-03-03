caab7df2884f0797fe1349a68e1b92df1adf1bf1
Larissa Borsari <larissaborsari@hotmail.com>
2023-03-03 09:47:57 -0300

    preparing episodes data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
 import { Inter } from '@next/font/google'
 import { GetStaticProps  } from 'next';
 import { api } from '../services/api';
+import { format, parseISO } from 'date-fns';
+import  ptBR from 'date-fns/locale/pt-BR'
+import { convertDurationToTImeString } from '../utils/convertDurationToTimeString';
 
 const inter = Inter({ subsets: ['latin'] });
 
 type Episode = {
-  episodes: Array<{
-    id: string,
-    title: string,
-    members: string,
-    //...
-  }>
-}
+    id: string;
+    title: string;
+    members: string;
+    published_at: string;
+    thumbnail: string;
+    description: string;
+    file: {
+      url: string;
+      type: string;
+      duration: number;
+    };
+  }
 
 type HomeProps = {
   episodes: Array<Episode>
@@ -33,11 +41,25 @@ export  const  getStaticProps: GetStaticProps= async() =>{
       _sort: 'published_at',
       _order: 'desc'
     }
-  });
+  })
+
+const episodes = data.map((e:Episode) => {
+  return {
+    id: e.id,
+    title: e.title,
+    thumbnail: e.thumbnail,
+    members: e.members,
+    publishedAt: format(parseISO(e.published_at), 'd MMM yy', { locale: ptBR}),
+    duration: Number(e.file.duration),
+    durationAsString: convertDurationToTImeString(Number(e.file.duration)),
+    description: e.description,
+    url: e.file.url,
+  }
+})
 
   return {
     props: {
-      episodes: data
+      episodes,
     },
     revalidate: 60 * 60 * 8
   }
