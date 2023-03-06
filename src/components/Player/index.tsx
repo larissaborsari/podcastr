e7debe64848d0b222cfab9ed82a836801b18d3d9
Larissa Borsari <larissaborsari@hotmail.com>
2023-03-05 21:55:56 -0300

    config progress slider

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css'
 
 
 export default function Player() {
@@ -12,46 +14,71 @@ export default function Player() {
         <div className={styles.playerContainer}>
 
             <header>
-                <img src="/playing.png" alt="Tocando agora" width="30px" />
-                <strong>Tocando Agora </strong>
+                <strong>    Tocando Agora </strong>
             </header>
 
           { episode ? (
+
             <div className={styles.currentEpisode}> 
                 <img width={250} height={250} src={episode.thumbnail} />
                 <strong>{episode.title}</strong>
                 <span>{episode.members}</span>
             </div>
+
           ) : (  
+
             <div className={styles.emptyPlayer}>
                 <strong>Selecione um podcast para ouvir</strong>
             </div>
+
             ) }
 
-            <footer className={styles.empty}>
+            <footer className={!episode ? styles.empty : ' '}>
+
                 <div className={styles.progess}>
+
                     <span>00:00</span>
                     <div className={styles.slider}>
-                    <div className={styles.emptySlider}/>
+                        { episode ? 
+                        ( 
+                            <Slider 
+                            trackStyle = {{ backgroundColor: '#04d361'}}
+                            railStyle = {{backgroundColor: '#9f75ff'}}
+                            handleStyle = {{borderColor: '#04d361', borderWidth: 4}}
+                            />
+                         ) : ( 
+                            <div className={styles.emptySlider}/>
+                        )}
+                
+
                     </div>
+
                     <span>00:00</span>
+
                 </div>
+
                 <div className={styles.buttons}>
+                    
                     <button type='button'>
                         <img src="/shuffle.png" alt="Embaralhar" width='18rem' />
                     </button>
+
                     <button type='button'>
                         <img src="/play-previous.png" alt="Tocar anterior" width='18rem'/>
                     </button>
+
                     <button type='button' className={styles.playButton}>
                         <img src="/play.png" alt="Tocar" width='20rem'/>
                     </button>
+
                     <button type='button'>
                         <img src="/play-next.png" alt="Tocar próxima" width='18rem' />
                     </button>
+
                     <button type='button'>
                         <img src="/repeat.png" alt="Repetir" width='18rem'/>
                     </button>
+
                 </div>
             </footer>
         </div>
